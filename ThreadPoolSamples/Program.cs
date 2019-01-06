5d8fd04df9c44c6c0fecea37222fd78082866429
thr828 <592040808@qq.com>
2019-01-06 23:08:43 +0800

    添加项目文件。

---
+++ ThreadPoolSamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Wrox.ProCSharp.Threading
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            int nWorkerThreads;  //最大工作线程
+            int nCompletionPortThreads;//最大I/O线程数
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionPortThreads);
+            Console.WriteLine("Max worker threads: {0}, I/O completion threads: {1}", nWorkerThreads, nCompletionPortThreads);
+
+            for (int i = 0; i < 3; i++)
+            {
+                ThreadPool.QueueUserWorkItem(JobForAThread);
+
+            }
+
+            Thread.Sleep(3000);
+
+            Console.ReadKey();
+        }
+
+
+        static void JobForAThread(object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("loop {0}, running inside pooled thread {1}", i,
+                   Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(50);
+            }
+
+        }
+    }
+}
